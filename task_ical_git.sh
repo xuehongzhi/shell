3a291fcccc9760695b1d225922853de164bc256d
xuehongzhi <rssdingyue4@gmail.com>
2018-02-26 14:19:13 +0800

    git任务导出

---
+++ task_ical_git.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+usr="$1"
+printf "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Google Inc//Google Calendar 70.9054//EN\n" > header.txt
+printf "END:VCALENDAR\n" > tail.txt
+(cat header.txt & git log --author=$1 --date=iso --pretty="%cd%x09%s" --since $2 --until $3\
+  |awk -F'\t' '{text=$2;\
+	  st=substr($1,12,5);dt=st+1;dt=dt""substr(st,4,4);\
+          sd =substr($1,1,10);gsub("-","",sd);\
+	  gsub(":","",st);gsub(":","",dt);\
+	  printf("BEGIN:VEVENT\nDTSTART;TZID=/Asia/Shanghai:%sT%s00\nDTEND;TZID=/Asia/Shanghai:%sT%s00\nSUMMARY:%s\nCLASS:PUBLIC\nEND:VEVENT\n",sd,st\
+	  ,sd,dt,text)\
+          }'\
+	  ) | cat - tail.txt  > $1_$2_$3.csv
